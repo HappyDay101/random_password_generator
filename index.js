1715ad3fa7cd024d9caaf0b0ac11843fa31e3254
Osama Suleiman <osama.suleiman97@gmail.com>
2025-01-19 17:35:33 +0900

    Create variables and DOM selections

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ let characters = {
     numbers: "1234567890",
     special: "!?{}-+=[]|~`@"
 };
+
+let passElOne = document.querySelector("#pass-el-one");
+let passElTwo = document.querySelector("#pass-el-two");
+let generateBtn = document.querySelector("#generate");
+let lengthValue = document.querySelector("#length-value");
+let includeLetters = document.querySelector("#include-letters");
+let includeNumbers = document.querySelector("#include-numbers");
+let includeSpecial = document.querySelector("#include-special");
+let selectAllBtn = document.querySelector("#select-all");
